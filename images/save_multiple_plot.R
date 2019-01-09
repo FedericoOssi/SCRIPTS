b6bf2aa252334fbeb410b5fa806b4957119a698f
Andrea Corradini <38738063+andreacorra@users.noreply.github.com>
2019-01-09 17:11:21 +0100

    Update save_multiple_plot.R

--- images/save_multiple_plot.R
+++ images/save_multiple_plot.R
@@ -1,7 +1,14 @@
-jpeg("multiple_plots.jpg")
+## Save multiple plots in R as image
+
+jpeg("multiple_plots.jpg",  # It works also with the functions png(), bmp(), tiff()
+      width = 1920,
+      height = 1080,
+      pointsize = 25, # size of the plotted text
+      quality = 100 # percentage, only for jpeg()
+    ) 
 
   par(mfrow=c(2,1))
-  hist(plot_1)
-  hist(plot_2)
+  hist("plot_1")
+  hist("plot_2")
   
 dev.off()
